059e21e703eec151248d4de31d793beee5d28933
sonagi784 <sonagi784@gmail.com>
2022-03-28 22:30:09 +0900

    fixed eslint jest path

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,8 @@ module.exports = {
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,

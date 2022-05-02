0c66b3c6485120dee4428ec4f2dd98d67d3dbce7
sonagi784 <sonagi784@gmail.com>
2022-05-02 18:02:40 +0900

    fixed some code

--- src/components/WebAPI/WebAPI.spec.ts
+++ src/components/WebAPI/WebAPI.spec.ts
@@ -2,19 +2,14 @@ import { shallowMount } from '@vue/test-utils';
 import WebAPI from './WebAPI.vue';
 
 describe('WebAPI.vue', () => {
-  const mockReload = jest.fn();
   Object.defineProperty(window, 'location', {
     configurable: true,
-    value: { reload: mockReload },
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
+    value: { reload: jest.fn() },
   });
 
   it('location.reload', async () => {
     const wrapper = shallowMount(WebAPI);
     await wrapper.find('button').trigger('click');
-    expect(mockReload).toBeCalled();
+    expect(window.location.reload).toBeCalled();
   });
 });

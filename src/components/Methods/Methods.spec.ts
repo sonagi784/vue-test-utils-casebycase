51dce3ed4d4ef9882c6e595c4860175e2175e655
sonagi784 <sonagi784@gmail.com>
2022-05-06 16:28:50 +0900

    add any type in class method component

--- src/components/Methods/Methods.spec.ts
+++ src/components/Methods/Methods.spec.ts
@@ -12,7 +12,7 @@ describe('Methods.vue', () => {
 
   it('method mocks in Class component', async () => {
     const spy = jest.spyOn(
-      MethodsClassComp.prototype.constructor.options.methods,
+      (MethodsClassComp.prototype.constructor as any).options.methods,
       'onClickMethod'
     );
     const wrapper = shallowMount(MethodsClassComp);
@@ -20,7 +20,7 @@ describe('Methods.vue', () => {
     expect(spy).toBeCalled();
   });
 
-  it('use method option (deprecated in VTU 2)', async () => {
+  it('use method option (deprecated in Vue 3)', async () => {
     const onClickMethod = jest.fn();
     const wrapper = shallowMount(Methods, {
       methods: {
